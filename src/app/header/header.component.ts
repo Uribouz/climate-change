36e2d3e67586b028bfa6bea9854d4d239290769d
Uribouz <vatcharitopas@gmail.com>
2024-09-02 21:12:10 +0700

    WIP implementing header.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { PrimaryComponent } from './primary/primary.component'
+import { SecondaryComponent } from './secondary/secondary.component'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [PrimaryComponent, SecondaryComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
